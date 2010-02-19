571b6e67d723c00a15a008d2907298e116839863
Anders Buus <buusmail@gmail.com>
2010-02-19 22:46:54 +0100

    QGstPlayer moved private class to public cpp file

--- src/player/qgstplayer.cpp
+++ src/player/qgstplayer.cpp
@@ -20,6 +20,10 @@
 #include "qgstplayer.h"
 #include "qgstplayer_p.h"
 
+/*
+ * --------------- Public Class --------------
+ */
+
 QGstPlayer::QGstPlayer(QObject *parent)
 	: QThread(parent),
 	d_ptr(new QGstPlayerPrivate())
@@ -79,4 +83,198 @@ bool QGstPlayer::enqueueNext(const QUrl &url)
 {
 	Q_D(QGstPlayer);
 	return d->enqueue(url);
+}
+
+/*
+ * --------------- Private Class --------------
+ */
+
+QGstPlayerPrivate::QGstPlayerPrivate()
+{
+	/* initialise gstreamer */
+	gst_init(NULL,NULL);
+
+	/* create gstreamer elements */
+	sink = gst_element_factory_make("autoaudiosink", "player");
+	pipeline = gst_element_factory_make("playbin", "player");
+	gst_element_link(pipeline, sink);
+
+	/* connect bus */
+	GstBus *bus;
+	bus = gst_element_get_bus(pipeline);
+	gst_bus_add_watch(bus,bus_callback,this);
+	g_object_unref(bus);
+}
+
+QGstPlayerPrivate::~QGstPlayerPrivate()
+{
+	g_object_unref(pipeline);
+	g_object_unref(sink);
+	g_main_loop_quit(gLoop);
+}
+
+void QGstPlayerPrivate::init()
+{
+	g_timeout_add(100,syncEventloop,this);
+	gLoop = g_main_loop_new(NULL,false);
+	g_main_loop_run(gLoop);
+}
+
+gboolean QGstPlayerPrivate::bus_callback(GstBus *bus,GstMessage *message, gpointer ptr)
+{
+	Q_UNUSED(bus);
+	QGstPlayerPrivate *thisPtr = static_cast<QGstPlayerPrivate*>(ptr);
+	Q_ASSERT(thisPtr);
+	switch(GST_MESSAGE_TYPE(message)) {
+		case GST_MESSAGE_EOS:
+			thisPtr->emitFinished();
+			break;
+		case GST_MESSAGE_DURATION:
+			thisPtr->updateDuration();
+			break;
+		case GST_MESSAGE_STATE_CHANGED:
+			GstState state;
+			gst_element_get_state(thisPtr->pipeline,&state,NULL,NULL);
+			thisPtr->setState(state);
+			break;
+	}
+	return true;
+}
+
+gboolean QGstPlayerPrivate::syncEventloop(gpointer ptr)
+{
+	Q_UNUSED(ptr);
+	QEventLoop qLoop;
+	qLoop.processEvents();
+
+	QGstPlayerPrivate *thisPtr = static_cast<QGstPlayerPrivate*>(ptr);
+	Q_ASSERT(thisPtr);
+
+	thisPtr->updatePosision();
+	
+	return true;
+}
+
+
+
+bool QGstPlayerPrivate::playUrl(const QUrl &url)
+{
+	gst_element_set_state(pipeline, GST_STATE_NULL);
+	QByteArray baUrl = url.toEncoded();
+	if(baUrl.isEmpty())
+	{
+		qDebug() << "playUrl: invalid url" << url;
+		return false;
+	}
+	const gchar *uri = baUrl.constData();
+	qDebug() << "QGstreamer play valid url:" << uri;
+	g_object_set(G_OBJECT(pipeline), "uri", uri, NULL);
+	updateDuration();
+	return gst_element_set_state(pipeline, GST_STATE_PLAYING);
+}
+
+bool QGstPlayerPrivate::play()
+{
+	updateDuration();
+	return gst_element_set_state(pipeline, GST_STATE_PLAYING);
+}
+
+bool QGstPlayerPrivate::pause()
+{
+	return gst_element_set_state(pipeline, GST_STATE_PAUSED);
+}
+
+bool QGstPlayerPrivate::stop()
+{
+	return gst_element_set_state(pipeline, GST_STATE_NULL);
+}
+
+void QGstPlayerPrivate::seek(const qint64 msec)
+{
+	Q_UNUSED(msec);
+	gst_element_seek(pipeline, 1.0, GST_FORMAT_TIME,
+		GST_SEEK_FLAG_FLUSH, GST_SEEK_TYPE_SET, msec * GST_MSECOND,
+		GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE);
+}
+
+bool QGstPlayerPrivate::enqueue(const QUrl &url)
+{
+	if(url.isValid())
+	{
+		QByteArray baUrl = url.toEncoded();
+		if(baUrl.isEmpty())
+		{
+			qDebug() << "enqueue invalid url" << url;
+			return false;
+		}
+		const gchar *uri = baUrl.constData();
+		g_object_set(G_OBJECT(pipeline), "uri", uri, NULL);
+		return true;
+	}
+	qDebug() << "enqeue failed" << url;
+	stop();
+	return false;
+}
+
+void QGstPlayerPrivate::setState(GstState state)
+{
+	Q_Q(QGstPlayer);
+	if( state == GST_STATE_PLAYING && m_state != 1)
+	{
+		m_state = 1;
+		emit q->stateChanged(m_state);
+		return;
+	}
+	else if( state == GST_STATE_PAUSED && m_state != 2)
+	{
+		m_state = 2;
+		emit q->stateChanged(m_state);
+		return;
+	}
+	else if( ( state == GST_STATE_VOID_PENDING || state == GST_STATE_NULL || state == GST_STATE_READY ) && m_state != 0)
+	{
+		m_state = 0;
+		emit q->stateChanged(m_state);
+		return;
+	}
+}
+
+void QGstPlayerPrivate::emitFinished()
+{
+	Q_Q(QGstPlayer);
+	emit q->finished();
+}
+
+void QGstPlayerPrivate::updateDuration()
+{
+	Q_Q(QGstPlayer);
+	GstFormat format = GST_FORMAT_TIME;
+	gint64 dur;
+	if(gst_element_query_duration(pipeline,&format,&dur))
+	{
+		durationSet = true;
+		m_duration = dur;
+		emit q->durationChanged(GST_TIME_AS_MSECONDS(m_duration));
+	}
+	else
+	{
+		durationSet = false;
+	}
+}
+
+void QGstPlayerPrivate::updatePosision()
+{
+	Q_Q(QGstPlayer);
+	if(!durationSet)
+	{
+		updateDuration();
+	}
+	GstFormat format = GST_FORMAT_TIME;
+	gint64 posision;
+	gst_element_query_position(pipeline,&format,&posision);
+	if(m_posision != posision)
+	{	
+		m_posision = posision;
+		emit q->timeChanged( GST_TIME_AS_MSECONDS(m_posision));
+	}
 }
